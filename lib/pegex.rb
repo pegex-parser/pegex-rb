b20bbc05dd859525b6e8bdbb3d292a786cdbed8b
Ingy döt Net <ingy@ingy.net>
2012-12-20 00:12:40 -0800

    v 0.02

--- lib/pegex.rb
+++ lib/pegex.rb
@@ -1,6 +1,4 @@
-module Pegex
-  VERSION = '0.0.1'
-end
+module Pegex;end
 
 require 'pegex/parser'
 require 'pegex/grammar'
@@ -10,9 +8,10 @@ def pegex grammar_text, receiver=nil
     require 'pegex/tree/wrap'
     receiver = Pegex::Tree::Wrap.new
   end
-  receiver = receiver.new if receiver.class == Class
-  return Pegex::Parser.new { |o|
-    o.grammar = Pegex::Grammar.new {|g| g.text = grammar_text}
-    o.receiver = receiver
-  }
+  receiver = receiver.new \
+    if receiver.class == Class
+  return Pegex::Parser.new do |p|
+    p.grammar = Pegex::Grammar.new {|g| g.text = grammar_text}
+    p.receiver = receiver
+  end
 end
