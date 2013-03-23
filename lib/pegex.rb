3e98697a37c19a68bdf35cad4c3530a5ab757720
Ingy döt Net <ingy@ingy.net>
2013-03-22 22:00:50 -0700

    match perl code

--- lib/pegex.rb
+++ lib/pegex.rb
@@ -3,15 +3,14 @@ module Pegex;end
 require 'pegex/parser'
 require 'pegex/grammar'
 
-def pegex grammar_text, receiver=nil
-  unless receiver
+def pegex grammar, receiver=nil
+  if not receiver
     require 'pegex/tree/wrap'
     receiver = Pegex::Tree::Wrap.new
   end
-  receiver = receiver.new \
-    if receiver.class == Class
+  receiver = receiver.new if receiver.class == Class
   return Pegex::Parser.new do |p|
-    p.grammar = Pegex::Grammar.new {|g| g.text = grammar_text}
+    p.grammar = Pegex::Grammar.new {|g| g.text = grammar}
     p.receiver = receiver
   end
 end
